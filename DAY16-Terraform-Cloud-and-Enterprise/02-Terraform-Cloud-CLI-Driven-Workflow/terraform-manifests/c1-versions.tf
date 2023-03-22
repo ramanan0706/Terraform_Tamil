e890284c210b0c85e849f3a9f40261b3acb478a2
“suresh.akidev” <sureshr.tamizan@gmail.com>
2023-03-23 00:21:10 +0530

    Skill Up Terraform

---
+++ DAY16-Terraform-Cloud-and-Enterprise/02-Terraform-Cloud-CLI-Driven-Workflow/terraform-manifests/c1-versions.tf
@@ -0,0 +1,26 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+}
+
+
+terraform {
+  cloud {
+    organization = "skillupwithstan"
+
+    workspaces {
+      name = "cli-workspace-test"
+    }
+  }
+}
